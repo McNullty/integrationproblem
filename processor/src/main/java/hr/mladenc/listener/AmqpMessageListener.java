7433e4fd35214abe990a58e77f10af9cc8410405
Mladen Čikara <mladen.cikara@gmail.com>
2014-12-08 22:05:01 +0100

    Added processor implementations

--- processor/src/main/java/hr/mladenc/listener/AmqpMessageListener.java
+++ processor/src/main/java/hr/mladenc/listener/AmqpMessageListener.java
@@ -3,6 +3,11 @@
  */
 package hr.mladenc.listener;
 
+import hr.mladenc.model.message.MessageFactory;
+import hr.mladenc.processor.MessageProcessor;
+
+import javax.annotation.Resource;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.Message;
@@ -19,15 +24,22 @@ import org.springframework.stereotype.Component;
 public class AmqpMessageListener implements MessageListener {
     private static final Logger log = LoggerFactory.getLogger(AmqpMessageListener.class);
 
+    @Resource(name = "${processor.name}")
+    private MessageProcessor processor;
+
+    private MessageFactory factor = new MessageFactory();
+
     /*
      * (non-Javadoc)
-     *
+     * 
      * @see org.springframework.amqp.core.MessageListener#onMessage(org.springframework.amqp.core.Message)
      */
     @Override
     public void onMessage(final Message message) {
         final String m = new String(message.getBody());
         AmqpMessageListener.log.debug("Listener got {}", m);
+
+        this.processor.process(this.factor.getMessage(m));
     }
 
 }
--- processor/src/main/java/hr/mladenc/listener/JmsMessageListener.java
+++ processor/src/main/java/hr/mladenc/listener/JmsMessageListener.java
@@ -3,6 +3,11 @@
  */
 package hr.mladenc.listener;
 
+import hr.mladenc.model.message.MessageFactory;
+import hr.mladenc.processor.MessageProcessor;
+
+import javax.annotation.Resource;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Profile;
@@ -19,11 +24,17 @@ public class JmsMessageListener {
 
     private static final Logger log = LoggerFactory.getLogger(JmsMessageListener.class);
 
+    @Resource(name = "${processor.name}")
+    private MessageProcessor processor;
+
+    private MessageFactory factor = new MessageFactory();
+
     @JmsListener(destination = "${queue.name}")
     public void process(final String data) {
 
         JmsMessageListener.log.debug("Listener got {}", data);
 
+        this.processor.process(this.factor.getMessage(data));
     }
 
 }
